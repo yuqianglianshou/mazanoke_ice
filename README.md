41a8a0ab1b5e2779e6849bb3a832563b28117bb5
civilblur <git.webd@extraramen.com>
2025-03-23 22:25:43 +0100

    feat: add manifest and service worker for pwa support

--- README.md
+++ README.md
@@ -14,3 +14,5 @@ MAZANOKE is a simple and lightweight image compressor that runs entirely in your
 MAZANOKE: [GNU General Public License v3.0](https://github.com/civilblur/mazanoke/blob/main/README.md)
 
 Browser Image Compression: [MIT License](https://github.com/Donaldcwl/browser-image-compression/blob/master/LICENSE)
+
+Geist, Geist Mono: [OFL License](https://github.com/vercel/geist-font/blob/main/LICENSE.txt)
---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'image-compression-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/fonts.css',
+  '/assets/css/variables.css',
+  '/assets/css/style.css',
+  '/assets/js/browser-image-compression.js',
+  '/assets/js/script.js',
+  '/assets/images/icon-192x192.png',
+  '/assets/images/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
