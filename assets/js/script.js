535cfe31883b204f03c23446ba5413eed16925df
civilblur <git.webd@extraramen.com>
2025-04-04 00:35:12 +0200

    refactor: adjust button responsiveness for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,6 +110,7 @@ function compressImageQueue() {
   const file = compressQueue[0];
   const i = compressProcessedCount;
   const options = createCompressionOptions((p) => onProgress(p, i, file.name), file);
+  // TODO: Display error message in UI
   imageCompression(file, options)
     .then((output) => handleCompressionResult(file, output))
     .catch((error) => console.error(error.message))
@@ -336,7 +337,7 @@ function handleCompressionResult(file, output) {
   outputDownload.download = outputFileNameText;
   outputDownload.innerHTML = `
     <svg height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16" style="color: currentcolor;"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.75 1V1.75V8.68934L10.7197 6.71967L11.25 6.18934L12.3107 7.25L11.7803 7.78033L8.70711 10.8536C8.31658 11.2441 7.68342 11.2441 7.29289 10.8536L4.21967 7.78033L3.68934 7.25L4.75 6.18934L5.28033 6.71967L7.25 8.68934V1.75V1H8.75ZM13.5 9.25V13.5H2.5V9.25V8.5H1V9.25V14C1 14.5523 1.44771 15 2 15H14C14.5523 15 15 14.5523 15 14V9.25V8.5H13.5V9.25Z" fill="currentColor"></path></svg>
-    <span>Download</span>
+    <span class="xs:hidden">Download</span>
   `;
   console.log("New image file: ", outputFileNameText);
 
